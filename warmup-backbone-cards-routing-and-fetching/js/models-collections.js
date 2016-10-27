0a12ead6a89829a33dd6b9d45da62d7805f98787
Jordan Wicker <Mike_company@yahoo.com>
2016-10-27 12:15:54 -0400

    first push

---
+++ warmup-backbone-cards-routing-and-fetching/js/models-collections.js
@@ -0,0 +1,14 @@
+var UserModel = Backbone.Model.extend({
+   idAttribute: 'email'
+})
+
+
+var UserCollection = Backbone.Collection.extend({
+   model: UserModel,
+   parse: function(rawJSONRes){
+      return rawJSONRes.results
+   },
+   initialize: function(qryStr){
+      this.url = "https://randomuser.me/api?"+qryStr
+   }
+})
